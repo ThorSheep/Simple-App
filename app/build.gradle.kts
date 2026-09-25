c6a3af22bb5984d313268145b65541288d04b44f
ThorSheep <121087258+ThorSheep@users.noreply.github.com>
2026-09-25 22:00:21 +0800

    chore: prepare v4.0.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "tw.thorsheep.simpleapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30200
-        versionName = "3.2.0"
+        versionCode = 40000
+        versionName = "4.0.0"
     }
     buildFeatures { compose = true; buildConfig = true }
     testOptions { unitTests.isIncludeAndroidResources = true }
